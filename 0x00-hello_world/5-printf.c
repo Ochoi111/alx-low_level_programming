54f63f1f61351518e77484efae2e53e693cb2d5f
Ochoi111 <victorsunday01@yahoo.com>
2023-02-16 15:07:33 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/** a program a grammer
- * followed by
- * a new line
+/**
+ * main- prints a string followed by a new line
+ * Return: 0
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
