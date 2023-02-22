76f2b6d98e6c9158c1884ffa2b12e9d288d28743
Ochoi111 <victorsunday01@yahoo.com>
2023-02-22 03:34:11 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,27 +7,28 @@
 
 void times_table(void)
 {
-	int a, b, pd;
+	int a, b, op;
 
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
 		for (b = 1; b <= 9; b++)
 		{
-			pd = a * b;
+			op = a * b;
 			_putchar(44);
 			_putchar(32);
-			if (pd <= 9)
+			if (op <= 9)
 			{
 				_putchar(32);
+				_putchar(op + 48);
 			}
 			else
 			{
-				_putchar((pd / 10) + 48);
-				_putchar((pd % 10) + 48);
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
 			}
 		}
-				_putchar('\n');
+		_putchar('\n');
 	}
 }
 
