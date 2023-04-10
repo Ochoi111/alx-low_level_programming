87ab1a73a7de403a28011ca000a11a5175a0ed39
Ochoi111 <victorsunday01@yahoo.com>
2023-04-10 07:52:18 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	int openfile, writefile, length = 0;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
@@ -21,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	openfile = open(filename, O_WRONLY | O_APPEND);
-	writefile = write(openfile, text_content, len);
+	writefile = write(openfile, text_content, length);
 
 	if (openfile == -1 || writefile == -1)
 		return (-1);
